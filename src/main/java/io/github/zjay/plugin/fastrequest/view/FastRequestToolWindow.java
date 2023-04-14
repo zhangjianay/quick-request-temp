4f209be7bf6eb273cad34d43f13cbcf0d3515a62
cqstt <384448255@qq.com>
2023-04-14 10:29:35 +0800

    优化 1、保存、重新生成数据按钮优化

--- src/main/java/io/github/zjay/plugin/fastrequest/view/FastRequestToolWindow.java
+++ src/main/java/io/github/zjay/plugin/fastrequest/view/FastRequestToolWindow.java
@@ -3395,7 +3395,7 @@ public class FastRequestToolWindow extends SimpleToolWindowPanel {
 
     private final class SaveRequestAction extends AnAction {
         public SaveRequestAction() {
-            super(MyResourceBundleUtil.getKey("SaveRequest"), MyResourceBundleUtil.getKey("SaveRequest"), AllIcons.Actions.MenuSaveall);
+            super(MyResourceBundleUtil.getKey("SaveRequest"), MyResourceBundleUtil.getKey("SaveRequest"), PluginIcons.ICON_SAVE);
         }
 
         @Override
